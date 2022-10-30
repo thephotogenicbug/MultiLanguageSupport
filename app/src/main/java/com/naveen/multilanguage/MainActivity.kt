1993e79e51fd4a22a3e80768ffd146108f49a68c
thephotogenicbug <navinkumarachari@gmail.com>
2022-10-30 16:07:52 +0530

    Add Kannada Language support

--- app/src/main/java/com/naveen/multilanguage/MainActivity.kt
+++ app/src/main/java/com/naveen/multilanguage/MainActivity.kt
@@ -2,10 +2,21 @@ package com.naveen.multilanguage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var show : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        show = findViewById(R.id.button)
+
+        show.setOnClickListener {
+            Toast.makeText(applicationContext,R.string.toast,Toast.LENGTH_LONG).show()
+        }
     }
 }
